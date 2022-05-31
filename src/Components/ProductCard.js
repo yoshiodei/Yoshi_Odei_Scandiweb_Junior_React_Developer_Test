f5c27fb1ad9d50cdabdb41523a91a598e700fc56
yoshiodei <yoshiodei@yahoo.com>
2022-05-31 21:45:51 +0000

    create product card component

---
+++ src/Components/ProductCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProductCard extends Component {
+    render() {
+        return (
+            <div className='card'>
+                
+            </div>
+        );
+    }
+}
+
+export default ProductCard;
