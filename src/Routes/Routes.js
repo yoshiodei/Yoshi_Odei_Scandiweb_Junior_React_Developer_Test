5486a426244f74ab6acf2edd2a8fd0096ecfbd54
yoshiodei <yoshiodei@yahoo.com>
2022-06-12 05:03:25 +0000

    modify product route to take param

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -3,21 +3,39 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from '../Navigation/Navbar';
 import CartPage from '../Pages/CartPage';
 import CategoryPage from '../Pages/CategoryPage';
+import PageNotFound from '../Pages/PageNotFound';
 import ProductsPage from '../Pages/ProductsPage';
 
 class RoutesList extends Component {
+
     render() {
         return (
             <Router>
                 <Navbar />
                 <Routes>
-                    <Route path="/" element={<CategoryPage />} />
-                    <Route path="/products" element={<ProductsPage />} />
-                    <Route path="/cart" element={<CartPage />} />
+                    <Route path='/' element={<CategoryPage />} />
+                    <Route path='/products/:id' element={<ProductsPage />} />
+                    <Route path='/cart' element={<CartPage />} />
+                    <Route path='*' element={<PageNotFound />} />
                 </Routes>
             </Router>    
         );
     }
 }
+
+// const ProductQuery = () => {
+//     return (
+//         <Query query={GET_PRODUCT_LIST}>
+//             {
+//                 ({loading, error, data, client}) => {
+//                     if (loading) return <div>Loading...{ console.log('data is', data, loading) }</div>
+//                     if (error) return <div>Error...{ console.log('error is', error) }</div>
+//                     return <div>Success...{ console.log('error is', data) }</div>
+//                     // <RoutesList client={client} data={data} />
+//                 }
+//             }
+//         </Query>
+//     )
+//   }
 
 export default RoutesList;
