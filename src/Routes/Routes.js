8ee2d8254770aafcffcb7831569f246d40118e5c
yoshiodei <yoshiodei@yahoo.com>
2022-06-18 09:51:09 +0000

    correct mistake in route name

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -4,7 +4,7 @@ import Navbar from '../Navigation/Navbar';
 import CartPage from '../Pages/CartPage';
 import CategoryPage from '../Pages/CategoryPage';
 import PageNotFound from '../Pages/PageNotFound';
-import ProductsPage from '../Pages/ProductsPage';
+import ProductDetailPage from '../Pages/ProductDetailPage';
 
 class RoutesList extends Component {
 
@@ -14,7 +14,7 @@ class RoutesList extends Component {
                 <Navbar />
                 <Routes>
                     <Route path='/' element={<CategoryPage />} />
-                    <Route path='/products/:id' element={<ProductsPage />} />
+                    <Route path='/products/:id' element={<ProductDetailPage />} />
                     <Route path='/cart' element={<CartPage />} />
                     <Route path='*' element={<PageNotFound />} />
                 </Routes>
@@ -22,20 +22,5 @@ class RoutesList extends Component {
         );
     }
 }
-
-// const ProductQuery = () => {
-//     return (
-//         <Query query={GET_PRODUCT_LIST}>
-//             {
-//                 ({loading, error, data, client}) => {
-//                     if (loading) return <div>Loading...{ console.log('data is', data, loading) }</div>
-//                     if (error) return <div>Error...{ console.log('error is', error) }</div>
-//                     return <div>Success...{ console.log('error is', data) }</div>
-//                     // <RoutesList client={client} data={data} />
-//                 }
-//             }
-//         </Query>
-//     )
-//   }
 
 export default RoutesList;
