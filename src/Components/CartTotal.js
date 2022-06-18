e42a9b3bc2b4f7c7082e50ec7424dcdc12bd9873
yoshiodei <yoshiodei@yahoo.com>
2022-06-18 10:33:21 +0000

    add function to find cart total

--- src/Components/CartTotal.js
+++ src/Components/CartTotal.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import '../Styles/CartTotal/CartTotal.styles.css';
 
 class CartTotal extends Component {
-    render() {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            tax: 0.21
+        }
+    }
+
+    render() {        
+        const {tax} = this.state;
+        const {cartItems:items,currencyLabel} = this.props;
+
+        const quantity = items.reduce((acc,item) => ( acc + item.quantity ),0);
+        const netTotal = items.reduce((acc,item) => ( acc + (item.prices[currencyLabel.index].amount * item.quantity) ),0);
+        const taxTotal = netTotal*tax;
+        const grossTotal = netTotal + taxTotal;
+         
+        
         return (
             <div className='cart-total'>
                 <div className='cart-total__text-div'>
-                    <p className='cart-total__text'>Tax 21%:</p>
-                    <p className='cart-total__text cart-total__text--boldened'>$42.00</p>
+                    <p className='cart-total__text'>{`Tax ${tax*100}%:`}</p>
+                    <p className='cart-total__text cart-total__text--boldened'>{`${currencyLabel.symbol}${taxTotal.toFixed(2)}`}</p>
                     <p className='cart-total__text'>Quantity:</p>
-                    <p className='cart-total__text cart-total__text--boldened'>3</p>
+                    <p className='cart-total__text cart-total__text--boldened'>{quantity}</p>
                     <p className='cart-total__text'>Total:</p>
-                    <p className='cart-total__text cart-total__text--boldened'>$200.00</p>
+                    <p className='cart-total__text cart-total__text--boldened'>{`${currencyLabel.symbol}${grossTotal.toFixed(2)}`}</p>
                 </div>
                 <button className='cart-total__button'>ORDER</button>
             </div>
@@ -19,4 +37,13 @@ class CartTotal extends Component {
     }
 }
 
-export default CartTotal;
+const mapStateToProps = (state) => {
+    return { 
+        cartItems : state.cartItems,
+        currencyLabel : state.currency
+    }
+}
+
+const mapDispatchToProps = {}; 
+
+export default connect(mapStateToProps,mapDispatchToProps)(CartTotal);
