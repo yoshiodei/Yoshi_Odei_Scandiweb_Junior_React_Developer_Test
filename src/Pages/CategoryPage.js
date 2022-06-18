fde1898274f470eb5eb6c0f469169420189c6a0e
yoshiodei <yoshiodei@yahoo.com>
2022-06-18 10:42:21 +0000

    add query component

--- src/Pages/CategoryPage.js
+++ src/Pages/CategoryPage.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import CategoryCard from '../Components/CategoryCard';
+import '../Styles/Category/Category.styles.css';
+import { GET_ALL_PRODUCTS } from '../EndPointQueries/queries';
+import { Query } from "@apollo/client/react/components";
+import ErrorPage from './ErrorPage';
 
 class CategoryPage extends Component {
     render() {
         return (
-            <div>
-                Category
+            <div className='category'>
+                <h1 className='category__header'>Category name</h1>
+                <section className='card-section'>
+                    <Query query={GET_ALL_PRODUCTS}>
+                        {({loading, data, error}) => {
+                            if(loading) return <div>...Loading</div>
+                            if(error) return <ErrorPage />
+                            const {products} = data.category;
+                            return products.map(product => (
+                                    <CategoryCard product={product} />
+                                ))                          
+                            }
+                        }
+                    </Query>
+                </section>
             </div>
         );
     }
