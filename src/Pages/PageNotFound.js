7bd17f146866717b6f682145a85e6742592fa85a
yoshiodei <yoshiodei@yahoo.com>
2022-06-18 10:22:22 +0000

    create error screen pages

---
+++ src/Pages/PageNotFound.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../Styles/ErrorScreens/ErrorScreens.css';
+import { Link } from 'react-router-dom'; 
+
+class PageNotFound extends Component {
+    render() {
+        return (
+            <section>
+                <div className='page-not-found'>
+                    <h1>Page Not Found!</h1>
+                    <Link to="/">
+                        <h3>Go Back To Home Page</h3>
+                    </Link>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default PageNotFound;
