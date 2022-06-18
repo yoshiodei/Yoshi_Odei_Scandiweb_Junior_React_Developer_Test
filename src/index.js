f2d56341bfc6f15f18a7742818a2affe33c6f029
yoshiodei <yoshiodei@yahoo.com>
2022-06-18 10:27:35 +0000

    create and add store to provider

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Routes from './Routes/Routes';
 import { ApolloProvider ,ApolloClient, InMemoryCache } from "@apollo/client";
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import { store } from './Redux/store';
 
 const client = new ApolloClient({
   uri: "http://localhost:4000",
@@ -15,7 +16,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-            <Routes />  
+      <Provider store={store}>  
+          <Routes />
+      </Provider>      
     </ApolloProvider>
   </React.StrictMode>
 );
