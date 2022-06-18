2942654c6937142ccda038da7adf8d5438ef3a48
yoshiodei <yoshiodei@yahoo.com>
2022-06-18 10:43:47 +0000

    include additional functions

--- src/Pages/ProductDetailPage.js
+++ src/Pages/ProductDetailPage.js
@@ -5,37 +5,72 @@ import { Query } from "@apollo/client/react/components";
 import ProductAttribute from '../Components/ProductAttribute';
 import { connect } from 'react-redux';
 import { addToCart } from '../Redux/action';
+import ErrorPage from './ErrorPage';
 
 class ProductDetailPage extends Component {
     constructor(props){
         super(props);
+
+        this.state = {
+            image: '',
+        }
+
+        this.list = {}
+} 
+
+    addProductToCart = (product) => {
+        let {selectedAttribute} = this.state;
+        let newProductId = product.id;
+        for(let key in selectedAttribute){
+            newProductId += `${key}${selectedAttribute[key]}`
+        }
+        let foundItemInCart = this.props.cartItems.find(item => item.id == newProductId);
+
+        if(!foundItemInCart){
+            let cartProduct = {...product, quantity: 1, id: newProductId, selectedAttribute};
+            this.props.addToCart(cartProduct);
+        } 
     }
 
-    addProductToCart = (product) =>{
-        let cartProduct = {...product, quantity: 1};
-        let foundItemInCart = this.props.cartItems.find(item => item.id == product.id);
-        if(!foundItemInCart) this.props.addToCart(cartProduct);
+    changeAttributeValue = (attributeObject) => {
+        let attributeSelect = {selectedAttribute: {...this.state.selectedAttribute, ...attributeObject}};
+        console.log('attributeObject', attributeSelect);
+        this.setState(attributeSelect)
     }
+
+    
 
     render() {
         const {currencyLabel} = this.props; 
 
         return (
             <>
-                <Query query={GET_PRODUCT_DETAIL} variables={{ product_id: window.location.pathname.split('/')[2] }} >
+                <Query query={GET_PRODUCT_DETAIL} 
+                       variables={{ product_id: window.location.pathname.split('/')[2] }}
+                       onCompleted={(data) =>
+                            // this.setState({ attributesList : data.product.attributes })
+                            
+                            data.product.attributes.forEach(attribute => {
+                                // this.list.push({[attribute.name] : attribute.items[0].displayValue})
+                                this.list = { ...this.list , [attribute.name] : attribute.items[0].displayValue}
+                                this.setState({selectedAttribute: this.list})
+                            })
+                            
+
+                       }>
                         {({loading, data, error}) => {
                             if(loading) return <div>...Loading</div>
-                            if(error) return <div>...Error {console.log('error',error)}</div>
+                            if(error) return <ErrorPage />
                             const {product} = data;
                             const price = product.prices?.find(price => price.currency.label === currencyLabel.label);
-                            return (
+                            return ( 
                                 <section className='products'>
                                     <div className='products__product-tile'>
                                         <div className='products__alternative-product-div'>
-                                            {
+                                            { (product.gallery.length > 1) &&
                                                 product.gallery.map((image,index)=>
                                                     (
-                                                        <div key={index} className={index == 0 ? 'products__alternative-image-div--selected' : 'products__alternative-image-div'} >
+                                                        <div key={index} className='products__alternative-image-div' onClick={() => this.setState({image})} >
                                                             <img src={image} alt='product' className='products__alternative-image' />
                                                         </div>
                                                     )
@@ -43,7 +78,7 @@ class ProductDetailPage extends Component {
                                             }
                                         </div>
                                         <div className='products__main-product-div'>
-                                            <img src={product.gallery[0]} alt='product' className='products__main-product-image'/>   
+                                            <img src={(this.state.image !== '') ? this.state.image : product.gallery[0] } alt='product' className='products__main-product-image'/>   
                                         </div>
                                     </div>
                                     <div className='product-detail'>
@@ -53,8 +88,13 @@ class ProductDetailPage extends Component {
                                         </div>
 
                                         {
-                                            product.attributes.map(attribute => (
-                                                <ProductAttribute attribute={attribute} />
+                                            product.attributes.map((attribute,index) => (
+                                                <ProductAttribute 
+                                                    attribute={attribute}   
+                                                    index={index} 
+                                                    changeAttributeValue={this.changeAttributeValue}
+                                                    // addToSelectedAttribute={this.addToSelectedAttribute} 
+                                                    />
                                             ))
                                         }
 
