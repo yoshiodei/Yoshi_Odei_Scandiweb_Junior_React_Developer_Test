164ea7735d8154091b137bcbf5f529dc1fa9857f
yoshiodei <yoshiodei@yahoo.com>
2022-06-20 21:40:52 +0000

    add session storage functionality

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -7,9 +7,18 @@ const initialState = {
 
 const reducer = (state = initialState , action) => {
     switch(action.type){
-        case ADD_TO_CART: return {...state, cartItems: [...state.cartItems, action.payload]};
-        case CHANGE_CURRENCY: return {...state, currency: action.payload};
-        case UPDATE_CART_LIST: return {...state, cartItems: action.payload};
+        case ADD_TO_CART:   let itemArray = [...state.cartItems, action.payload];
+                            window.sessionStorage.setItem('cartItem', JSON.stringify(itemArray));
+                            return {...state, cartItems: itemArray};
+
+        case CHANGE_CURRENCY:   let currency = action.payload;
+                                window.sessionStorage.setItem('currency', JSON.stringify(currency));
+                                return {...state, currency};
+
+        case UPDATE_CART_LIST:  let updatedItemArray = action.payload;
+                                window.sessionStorage.setItem('cartItem', JSON.stringify(updatedItemArray));
+                                return {...state, cartItems: action.payload};
+
         default: return state; 
     }
 }
