05045b206801644970f002083a686b14fd70cf81
yoshiodei <yoshiodei@yahoo.com>
2022-06-30 21:05:14 +0000

    make coreection to category card button

--- src/Components/CategoryCard.js
+++ src/Components/CategoryCard.js
@@ -21,19 +21,23 @@ class CategoryCard extends Component {
         const {product,currencyLabel} = this.props; 
         const price = product.prices?.find(price => price.currency.label === currencyLabel.label)
         return (
-            <Link  to={(product.inStock) ? `products/${product.id}` : `/`}  className='card' key={product.id} >
-                <div className='card__image-div'>
-                    <img src={product.gallery[0]} alt='product' className='card__image' />
-                    { product.inStock && <div className='card__button-div' onClick={()=> this.addProductToCart(product)}>
-                        <img src={cart_white} alt="cart" className="card__button-image" />    
-                    </div>}
-                </div>
-                <div className="card__text-div">
-                    <p className="card__item-name">{product.name}</p>
-                    <p className="card__item-price">{`${price?.currency?.symbol}${price?.amount}`}</p>
-                </div>
-                { !product.inStock && <div className='card__out-of-stock'><h2>OUT OF STOCK</h2></div>}
-            </Link>
+            <div className='card-div'>
+                <Link  to={(product.inStock) ? `products/${product.id}` : `/`}  className='card' key={product.id} >
+                    <div className='card__image-div'>
+                        <img src={product.gallery[0]} alt='product' className='card__image' />
+                    </div>
+                    <div className="card__text-div">
+                        <p className="card__item-name">{product.name}</p>
+                        <p className="card__item-price">{`${price?.currency?.symbol}${price?.amount}`}</p>
+                    </div>
+                    { !product.inStock && <div className='card__out-of-stock'><h2>OUT OF STOCK</h2></div>}
+                </Link>
+                { 
+                    product.inStock && <div className='card-div__button-div' onClick={()=> this.addProductToCart(product)}>
+                            <img src={cart_white} alt="cart" className="card__button-image" />    
+                    </div>
+                }
+            </div>
         );
     }
 }
