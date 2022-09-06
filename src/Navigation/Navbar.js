8070ebad607488bd751457c3780ef355d5f3c44f
yoshiodei <yoshiodei@yahoo.com>
2022-09-06 15:12:43 +0000

    modify navbar component and styles

--- src/Navigation/Navbar.js
+++ src/Navigation/Navbar.js
@@ -8,7 +8,9 @@ import CartModalCard from '../Components/CartModalCard';
 import CurrencySwitcher from '../Components/CurrencySwitcher';
 import {connect} from 'react-redux';
 import CartModalTotal from '../Components/CartModalTotal';
-import { updateCartList, changeCurrency } from '../Redux/action';
+import { updateCartList, changeCurrency, changeCategory } from '../Redux/action';
+import { GET_CATEGORIES } from '../EndPointQueries/queries';
+import { Query } from "@apollo/client/react/components";
 
 
 class Navbar extends Component {
@@ -24,9 +26,10 @@ class Navbar extends Component {
         if(currency) this.props.changeCurrency(JSON.parse(currency));
 
         const [cartOverlayModal] = document.getElementsByClassName('navbar__cart-overlay-div');
+        const [cartOverlayModaContainer] = document.getElementsByClassName('navbar__cart-overlay-container');
         const [currencySwitcherCaret] = document.getElementsByClassName('navbar__button-icon');
         const [currencySwitcher] = document.getElementsByClassName('navbar__currency-switcher-div');
-        this.setState({cartOverlayModal, currencySwitcher, currencySwitcherCaret});
+        this.setState({cartOverlayModal, currencySwitcher, currencySwitcherCaret,cartOverlayModaContainer});
     }
 
     toggleModal = () => {
@@ -42,21 +45,40 @@ class Navbar extends Component {
     }
 
     preventPropagation = (e) => {
-        if(e.target === this.state.cartOverlayModal) this.toggleModal();
+        if(e.target === this.state.cartOverlayModal || e.target === this.state.cartOverlayModaContainer) this.toggleModal();
     }
 
     render() {
-        const { cartItems,currencyLabel } = this.props;
-
+        const { cartItems,currencyLabel,changeCategory } = this.props;
+        const quantity = cartItems.reduce((acc,item)=>(acc + item.quantity),0)
+        const category = window.location.href.split('3000/')[1] || 'all';
         return (
+            
             <nav className='navbar'>
-               <ul className='navbar__list'>
-                   <li className='navbar__list-item navbar__list-item--active'>ALL</li>
-                   <li className='navbar__list-item'>CLOTHES</li>
-                   <li className='navbar__list-item'>TECH</li>
-               </ul>
+               <Query query={GET_CATEGORIES}>
+                    {({loading, data, error}) => {
+                            if(loading) return (
+                            <ul className='navbar__list'>
+                            <li className='navbar__list-item'></li>
+                            </ul>);
+                            if(error) return null;
+                            if(data) return (
+                                <ul className='navbar__list'>
+                                {
+                                    data.categories.map(
+                                        (item, index) => (
+                                            <li className={item.name === category ? 'navbar__list-item navbar__list-item--active': 'navbar__list-item'} key={index} >
+                                                <span className='navbar__list-link' onClick={() => window.location.assign(`/${item.name}`)} >{item.name}</span>
+                                            </li>
+                                        )
+                                    )
+                                }    
+                                </ul>);
+                    }}
+               </Query> 
+               
 
-               <Link to="/" className='navbar__logo'>
+               <Link to="/all" className='navbar__logo'>
                     <img src={logo} alt='logo'/> 
                </Link> 
 
@@ -67,7 +89,7 @@ class Navbar extends Component {
                     </div>
                     <div className='navbar__cart-button' onClick={this.toggleModal}>
                         <div className='navbar__cart-button-badge'>
-                            <p className='navbar__cart-button-badge-text'>{cartItems.length}</p>
+                            <p className='navbar__cart-button-badge-text'>{quantity}</p>
                         </div>
                        <img src={cart} className='navbar__button-icon' alt='cart' />
                     </div>
@@ -76,24 +98,27 @@ class Navbar extends Component {
                <CurrencySwitcher toggleCurrencySwitcher={this.toggleCurrencySwitcher} />
 
                <div className='navbar__cart-overlay-div' onClick={(e) => this.preventPropagation(e)}>
-                    <div className='navbar__cart-overlay-modal'>
-                        <h3 className='navbar__modal-title'><span className='navbar__modal-title navbar__modal-title--boldened'>
-                            My Bag, </span> {cartItems.length} {`item${(cartItems.length > 1)? 's':''}`}
-                        </h3>
-                            {(cartItems.length !== 0) ?
-                                <>{(cartItems.map(item => (
-                                    <CartModalCard item={item} />
-                                )))
-                                }<CartModalTotal /></> :
-                                <h3 className='navbar__cart-empty-header'>Cart is Empty</h3>
-                            }
-                        <div className='navbar__modal-button-div'>
-                            <Link to='/cart' className='navbar__modal-button' onClick={this.toggleModal}>VIEW BAG</Link>
-                            <button className='navbar__modal-button navbar__modal-button--green'onClick={this.toggleModal}>CHECK OUT</button>
+                    <div className='navbar__cart-overlay-container'>
+                        <div className='navbar__cart-overlay-modal'>
+                            <h3 className='navbar__modal-title'><span className='navbar__modal-title navbar__modal-title--boldened'>
+                                My Bag, </span> {quantity} {`item${(quantity > 1)? 's':''}`}
+                            </h3>
+                                {(cartItems.length !== 0) ?
+                                    <>{(cartItems.map(item => (
+                                        <CartModalCard item={item} />
+                                    )))
+                                    }<CartModalTotal /></> :
+                                    <h3 className='navbar__cart-empty-header'>Cart is Empty</h3>
+                                }
+                            <div className='navbar__modal-button-div'>
+                                <Link to='/cart' className='navbar__modal-button' onClick={this.toggleModal}>VIEW BAG</Link>
+                                <button className='navbar__modal-button navbar__modal-button--green'onClick={this.toggleModal}>CHECK OUT</button>
+                            </div>
                         </div>
                     </div>
                </div>
             </nav>
+           
         );
     }
 }
@@ -101,10 +126,11 @@ class Navbar extends Component {
 const mapStateToProps = (state) => {
     return { 
         cartItems : state.cartItems,
-        currencyLabel : state.currency
+        currencyLabel : state.currency,
+        category: state.category,
      }
 }
 
-const mapDispatchToProps = { updateCartList, changeCurrency }
+const mapDispatchToProps = { updateCartList, changeCurrency, changeCategory }
 
 export default connect(mapStateToProps,mapDispatchToProps)(Navbar);
